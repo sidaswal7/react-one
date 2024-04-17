7c3f67ad527ae22a6a260b2e4e501edaae8a8758
sidaswal7 <siddhantaswal7@gmail.com>
2024-04-17 16:41:31 +0530

    used custom component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   return (
     <div>
      <h1>Let's get started</h1>
      <p>I am learning react</p>
-     <p>hello world</p>
+     <ExpenseItem/>
     </div>
   );
 }
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem(){
+    return(
+        <div>
+            <p>Food - $5</p>
+            <p>Petrol - $20</p>
+            <p>Movie - $10</p>
+        </div>
+    )
+}
+
+export default ExpenseItem;
