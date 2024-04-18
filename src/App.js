a000a921f6b1252c8ef96c87a6b8f937093114a2
sidaswal7 <siddhantaswal7@gmail.com>
2024-04-18 11:54:55 +0530

    added data through props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+
+  const expenses = [
+    {id:1, date: new Date(2023, 7, 15), title:"Insurance",amount: 100,location:"Bangalore"},
+    {id:2, date: new Date(2023, 3, 23), title:"Book",amount: 10,location:"Delhi"},
+    {id:3, date: new Date(2023, 11, 29), title:"Pen",amount: 1,location:"Hyderabad"},
+    {id:4, date: new Date(2023, 1, 8), title:"Laptop",amount: 200,location:"Mumbai"}
+  ]
   return (
     <div>
      <h1>Let's get started</h1>
      <p>I am learning react</p>
-     <ExpenseItem/>
+     {
+      expenses.map((expense)=> {
+        return (
+          <ExpenseItem
+          key={expense.id}
+          date={expense.date}
+          title={expense.title}
+          amount={expense.amount}
+          location={expense.location}
+          /> 
+        )
+      })
+     
+     }
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,14 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
-    const expenseDate = new Date(2023, 7, 15).toISOString();
-    const expenseTitle = "Insurance";
-    const expenseAmount = 50;
-    const expenseLocation = "Bangalore";
-  
+function ExpenseItem(props) {
+
     return (
       <div className="expense-item">
-        <div>{expenseDate}</div>
-        <div className="expense-item__location">{expenseLocation}</div>
+        <div>{props.date.toISOString()}</div>
+        <div className="expense-item__location">{props.location}</div>
         <div className="expense-item__description">
-          <h2>{expenseTitle}</h2>
-          <div className="expense-item__price">${expenseAmount}</div>
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
         </div>
       </div>
     );
